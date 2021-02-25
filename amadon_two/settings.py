11cd2b245fbce9f2c17bc2eb6b2fb546a1dfe817
Caleb A Comstock <c_comstock@rocketmail.com>
2021-02-25 11:11:10 -0800

    updated form processing to get price from db and redirect at checkout

--- amadon_two/settings.py
+++ amadon_two/settings.py
@@ -31,13 +31,13 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'poorly_coded_store',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'poorly_coded_store'
 ]
 
 MIDDLEWARE = [
--- poorly_coded_store/views.py
+++ poorly_coded_store/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from decimal import Decimal
 from .models import Order, Product
 
 def index(request):
@@ -8,9 +9,29 @@ def index(request):
     return render(request, "store/index.html", context)
 
 def checkout(request):
+    item_from_form = Product.objects.get(id=int(request.POST['product_id']))
     quantity_from_form = int(request.POST["quantity"])
-    price_from_form = float(request.POST["price"])
-    total_charge = quantity_from_form * price_from_form
+    # price_from_form = float(request.POST["price"])
+    total_charge = item_from_form.price * quantity_from_form
     print("Charging credit card...")
     Order.objects.create(quantity_ordered=quantity_from_form, total_price=total_charge)
-    return render(request, "store/checkout.html")
+    request.session['last_charged_amount'] = str(total_charge)
+    if 'total_items_ordered' in request.session:
+        request.session['total_items_ordered'] = str(int(request.session['total_items_ordered']) + quantity_from_form)
+    else:
+        request.session['total_items_ordered'] = str(quantity_from_form)
+    if 'total_money_spent' in request.session:
+        request.session['total_money_spent'] = str(Decimal(request.session['total_money_spent']) + total_charge)
+    else:
+        request.session['total_money_spent'] = str(total_charge)
+    return redirect('/confirmed')
+
+def confirmed(request):
+    context = {}
+    if 'last_charged_amount' in request.session:
+        context['amount_charged'] = request.session['last_charged_amount']
+    if 'total_items_ordered' in request.session:
+        context['total_items'] = request.session['total_items_ordered']
+    if 'total_money_spent' in request.session:
+        context['total_spent'] = request.session['total_money_spent']
+    return render(request, "store/success.html", context)
